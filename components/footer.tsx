abc16ba6d4c3eaafd64f8c89945c724c3eb41151
Aarti-Mern-Stack <aarti.verma@designersx.com>
2025-06-04 12:43:33 +0530

    [AV] updated code here (AV1.1)

--- components/footer.tsx
+++ components/footer.tsx
@@ -38,7 +38,7 @@ export function Footer({ footerData }: FooterProps) {
   return (
     <footer className="bg-gray-900 text-white pt-16 pb-8">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 mb-12">
           {/* Footer Logo */}
           <div>
             {footerData?.footerLogo && (
