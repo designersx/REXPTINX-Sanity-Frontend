c3c3adcb0d961b79846231cede9d5fbd2b4e7fc7
Aarti-Mern-Stack <aarti.verma@designersx.com>
2025-06-07 16:27:39 +0530

    [AV] updated code here (AV1.1)

--- components/hero-section2.tsx
+++ components/hero-section2.tsx
@@ -88,6 +88,12 @@ export function HeroSection2(props: HeroSection2Props) {
   };
 
   useEffect(() => {
+    const isMobile = window.innerWidth <= 768;
+
+    if (isMobile) {
+      console.log("Mobile detected, not loading the script.");
+      return;
+    }
     let script: HTMLScriptElement | null = null;
 
     const removeExistingWidgetElements = () => {
@@ -130,7 +136,6 @@ export function HeroSection2(props: HeroSection2Props) {
       removeExistingWidgetElements();
     };
   }, []);
-
 
   const serializers = {
     marks: {
